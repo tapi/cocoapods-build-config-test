d409111970abd23355e8d7fdbe81c7cd410c123c
Paddy O'Brien <paddyobrien@Paddys-MacBook-Pro.local>
2015-07-19 12:56:54 -0400

    Initial Commit

---
+++ Pods-Target/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pods-Target
+//
+//  Created by Paddy O'Brien on 2015-07-19.
+//  Copyright (c) 2015 Paddy O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
