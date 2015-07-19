7f00be587cfeceda202b0b35dfe9f31b9ddaea98
Paddy O'Brien <paddyobrien@Paddys-MacBook-Pro.local>
2015-07-19 13:20:44 -0400

    fixed snafu

---
+++ Pods/Target Support Files/DCIntrospect-ARC/DCIntrospect-ARC-umbrella.h
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+
+#import "DCCrossHairView.h"
+#import "DCFrameView.h"
+#import "DCIntrospect.h"
+#import "DCIntrospectSettings.h"
+#import "DCStatusBarOverlay.h"
+#import "DCTextView.h"
+#import "UIView+DCAdditions.h"
+
+FOUNDATION_EXPORT double DCIntrospect_ARCVersionNumber;
+FOUNDATION_EXPORT const unsigned char DCIntrospect_ARCVersionString[];
+
---
+++ Pods/Target Support Files/Pods-Pods-Target/Pods-Pods-Target-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Pods_TargetVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Pods_TargetVersionString[];
+
